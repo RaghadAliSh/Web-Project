769c839af4be74baf2d213b732db59b6475a4647
Raghad Sad Aldeen <151654896+Raghad-sad-Aldeen@users.noreply.github.com>
2023-12-28 14:47:48 +0200

    This's create post component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+// import './App.css';
+
+import Createpost from "./createPost_component/CreatePost";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Createpost/>
+      
+
+    </div>
+  );
+}
+
+export default App;
